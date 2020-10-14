8ad14f421ee7b414e8ab0e19b8a1780192b7aa16
Adam Johnson <me@adamj.eu>
2020-10-14 09:16:26 +0100

    Slides and gather.town room for 2020-10-13

---
+++ gather.town/tilesets/pony.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="pony" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <image source="pony.png" width="224" height="160"/>
+</tileset>
